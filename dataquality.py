4f472a95639da2936f1ddcfc4cc11013dce85b8c
BrenoLima54 <breno54lima54@gmail.com>
2024-09-29 17:25:55 -0300

    Limpeza e verificação da versão final do codigo

--- dataquality.py
+++ dataquality.py
@@ -1,17 +1,12 @@
-# Passos para desenvolver o projeto
-
-# 1 . Importar bibliotecas
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
 from tabulate import tabulate
 from IPython.display import display_markdown 
-# import seaborn as sns # caso seja necessário para gráficos
 
-# 2. Cirar classe DataQuality
 class DataQuality:
     def __init__(self, diretorio:str) -> None:
-        self.df = pd.read_csv(diretorio) # leitura do diretorio
+        self.df = pd.read_csv(diretorio)
         self.df_cat = self.df.select_dtypes(exclude=np.number)
         self.df_num = self.df.select_dtypes(include=np.number)
 
@@ -107,16 +102,3 @@ class DataQuality:
 
         self.numerical_analyzes()
         self.categorical_analyzes()
-
-    # 3. Criar métodos: 
-    # Mostrar Cabeçalho (método head())
-    # Mostrar fim do df (método tail())
-    # Somatória dos valores nulos
-    # Somatória dos valores únicos
-    # Análise dos dados categóricos geral ou por coluna (podemos definir a melhor forma)
-        # df_cat.value_counts()
-    # Análise dos dados numéricos 
-        # df_num.describe()
-        # df_num.corr() -> da pra fazer heatmap
-    # Gráficos
-    # Método para gerar relatório completo

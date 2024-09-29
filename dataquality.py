5d9d82f707bb82426d384e9f4d49e57f91074860
BrenoLima54 <breno54lima54@gmail.com>
2024-09-29 15:10:35 -0300

    Alteração dos metodos iniciais e importação das bibliotecas necessarias

--- dataquality.py
+++ dataquality.py
@@ -4,7 +4,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
-import tabulate as tab # essa bibliote permite formatar o print do df
+from tabulate import tabulate
+from IPython.display import display_markdown 
 # import seaborn as sns # caso seja necessário para gráficos
 
 # 2. Cirar classe DataQuality
@@ -14,30 +15,32 @@ class DataQuality:
         self.df_categ = self.df.select_dtypes(exclude=np.number)
         self.df_num = self.df.select_dtypes(include=np.number)
 
-    def header(self, n=5) -> None:
-        print("Dataframe - Cabeçalho:")
-        head_df = self.df.head(n)
-        print(tab.tabulate(head_df, headers='keys', showindex='always' , tablefmt='fancy_grid'))
+    def firts_rows(self, n=5):
+        display_markdown(f'''### Primeiras {n} linhas:''', raw=True)
+        head_df = self.df.head(n).reset_index()
+        print(tabulate(head_df, headers="keys", tablefmt="fancy_grid"))
+    
+    def last_rows(self, n=5):
+        display_markdown(f'''### Últimas {n} linhas:''', raw=True)
+        tail_df = self.df.tail(n)
+        print(tabulate(tail_df, headers="keys", tablefmt="fancy_grid"))
 
-        print("\nDataframe Categórico:")
-        categ_df = self.df_categ.head(n)
-        print(tab.tabulate(categ_df, headers='keys', showindex='always' , tablefmt='fancy_grid'))
-
-        print("\nDataframe Numérico:")
-        num_df = self.df_num.head(n)
-        print(tab.tabulate(num_df, headers='keys', showindex='always' , tablefmt='fancy_grid'))
+    def sample_rows(self, n=5):
+        display_markdown(f'''### Amostra de {n} linhas:''', raw=True)
+        sample_rows = self.df.sample(n, random_state=np.random.seed(42))
+        print(tabulate(sample_rows, headers="keys", tablefmt="fancy_grid"))
     
     def count_nulls(self) -> None:
         print("Contagem de valores nulos:")
         df_nulos = self.df.isnull().sum().reset_index()
         df_nulos.columns = ['Coluna', 'Quantidade']
-        print(tab.tabulate(df_nulos, headers='keys', showindex='never' , tablefmt='fancy_grid'))
+        print(tabulate(df_nulos, headers='keys', showindex='never' , tablefmt='fancy_grid'))
 
     def count_unique(self) -> None:
         print("Contagem de valores únicos:")
         df_unicos = self.df.nunique().reset_index()
         df_unicos.columns = ['Coluna', 'Quantidade']
-        print(tab.tabulate(df_unicos, headers='keys', showindex='never' , tablefmt='fancy_grid'))
+        print(tabulate(df_unicos, headers='keys', showindex='never' , tablefmt='fancy_grid'))
 
         
     # 3. Criar métodos: 
